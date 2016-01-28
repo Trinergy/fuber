a92d390dd31858a56035a1634237147dac41d013
vagrant <vagrant@lighthouse-labs>
2016-01-28 22:39:36 +0000

    added routes to action.rb

---
+++ sinatra-skeleton/app/order_actions.rb
@@ -0,0 +1,32 @@
+##################
+####ORDER GETS####
+##################
+
+get '/orders/menu' do 
+
+  erb :'orders/menu'
+end
+
+get '/orders/new' do
+
+  erb :'orders/new'
+end
+
+get '/orders' do 
+
+  erb :'orders/index'
+end
+
+get '/orders/:order_id' do 
+
+  erb :'orders/show'
+end
+
+
+####################
+####ORDER POSTS#####
+####################
+
+post '/orders'
+
+end
