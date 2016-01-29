e5c48545157cdb7c83a506391ce1f4e57abc494e
Trinergy <wu.kenny1221@gmail.com>
2016-01-29 03:48:47 +0000

    add login

--- sinatra-skeleton/app/actions.rb
+++ sinatra-skeleton/app/actions.rb
@@ -1,6 +1,14 @@
 require_relative 'order_actions'
 
 
+helpers do
+  
+  def current_user
+    @current_user ||= User.find(session[:user_id])
+  end
+
+end
+
 ##############
 ####GETS######
 ##############
@@ -14,7 +22,6 @@ get '/user' do
 end
 
 get '/session/new' do
-
   erb :'session/new'
 end
 
@@ -23,7 +30,11 @@ end
 ##############
 
 post '/session' do
-
-
-  erb :'session/new'
+  user = User.find_by(username: params[:username], password: params[:password])
+  if user
+    session[:user_id] = user.id
+    redirect "/"
+  else
+    erb :'session/new'
+  end
 end
